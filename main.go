90d4b074d4bbaf89939225ea82ee30f62e4be0dc
Hans Missenheim <hansmissenheim@gmail.com>
2021-01-19 01:57:22 -0500

    Create infoCollector to save player name in struct

--- main.go
+++ main.go
@@ -10,23 +10,40 @@ const (
 	baseURL = "https://www.sports-reference.com/cfb/players/%v-index.html"
 )
 
+type player struct {
+	name string
+}
+
 func main() {
 	playerCollector := colly.NewCollector()
+	infoCollector := colly.NewCollector()
 
 	// Visits each player's individual page
 	playerCollector.OnHTML(".section_content > p", func(e *colly.HTMLElement) {
-		link := e.ChildAttr("a", "href")
-		e.Request.Visit(link)
+		link := e.Request.AbsoluteURL(e.ChildAttr("a", "href"))
+		infoCollector.Visit(link)
 	})
 
 	// Visits the next page for the current letter in the index
 	playerCollector.OnHTML(".next", func(e *colly.HTMLElement) {
-		link := e.Attr("href")
-		e.Request.Visit(link)
+		playerURL := e.Attr("href")
+		e.Request.Visit(playerURL)
 	})
 
 	playerCollector.OnRequest(func(r *colly.Request) {
 		fmt.Println("Visiting", r.URL)
+	})
+
+	infoCollector.OnHTML("div#info", func(e *colly.HTMLElement) {
+		tempPlayer := player{}
+		e.ForEach("h1 > span", func(_ int, el *colly.HTMLElement) {
+			tempPlayer.name = el.Text
+			fmt.Println(tempPlayer)
+		})
+	})
+
+	infoCollector.OnRequest(func(r *colly.Request) {
+		fmt.Println("Info visiting", r.URL)
 	})
 
 	// Iterates through each letter of the player index
@@ -34,4 +51,5 @@ func main() {
 		link := fmt.Sprintf(baseURL, string('a'-1+i))
 		playerCollector.Visit(link)
 	}
+	playerCollector.Visit("https://www.sports-reference.com/cfb/players/a-index-2.html")
 }
