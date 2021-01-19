9e784aa29f1ac3167c5ceacf9ff4429aee7da118
Hans Missenheim <hansmissenheim@gmail.com>
2021-01-18 20:33:34 -0500

    Create collector to visit each player's pager

--- main.go
+++ main.go
@@ -6,12 +6,32 @@ import (
 	"github.com/gocolly/colly/v2"
 )
 
-func main() {
-	c := colly.NewCollector()
+const (
+	baseURL = "https://www.sports-reference.com/cfb/players/%v-index.html"
+)
 
-	c.OnRequest(func(r *colly.Request) {
+func main() {
+	playerCollector := colly.NewCollector()
+
+	// Visits each player's individual page
+	playerCollector.OnHTML(".section_content > p", func(e *colly.HTMLElement) {
+		link := e.ChildAttr("a", "href")
+		e.Request.Visit(link)
+	})
+
+	// Visits the next page for the current letter in the index
+	playerCollector.OnHTML(".next", func(e *colly.HTMLElement) {
+		link := e.Attr("href")
+		e.Request.Visit(link)
+	})
+
+	playerCollector.OnRequest(func(r *colly.Request) {
 		fmt.Println("Visiting", r.URL)
 	})
 
-	c.Visit("https://www.sports-reference.com/cfb/")
+	// Iterates through each letter of the player index
+	for i := 1; i < 27; i++ {
+		link := fmt.Sprintf(baseURL, string('a'-1+i))
+		playerCollector.Visit(link)
+	}
 }
